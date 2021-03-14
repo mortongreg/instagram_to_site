09e0395f5b23c9ebd16b3f8a71f4b478610985e4
mortongreg <morton.greg@gmail.com>
2021-03-14 12:47:41 -1000

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config(object):
-    SECRET_KEY = "2bdfb07022f2438bb35bd7b7ee6697db"
-    SITE_NAME = "https://sitegen.mortoch.com"
-    INSTAGRAM_CLIENT_ID = "737101987128986"
-    INSTAGRAM_CLIENT_SECRET = "e2c5cb5757af6389ee0e2b5fb0d19b71"
-    INSTAGRAM_REDIRECT_URI = "https://sitegen.mortoch.com/authorize"
+    SECRET_KEY = ""
+    SITE_NAME = ""
+    INSTAGRAM_CLIENT_ID = ""
+    INSTAGRAM_CLIENT_SECRET = ""
+    INSTAGRAM_REDIRECT_URI = ""
                              
